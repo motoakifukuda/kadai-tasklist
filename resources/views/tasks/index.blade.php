36110fc843e134f4ef7a43c58724bb4e248ed257
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-11 02:55:01 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,11 +7,11 @@
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</li>
             @endforeach
         </ul>
     @endif
     
-    {!! link_to_route('tasks.create', '新規タスクの作成', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.create', '新規タスクの作成') !!}
     
 @endsection
