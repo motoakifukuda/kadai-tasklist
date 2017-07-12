6a30990c2f3175198994912b8c77d995152ce5e8
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-12 01:08:24 +0000

    bootstrap design3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,25 +5,23 @@
     <h1>タスク新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12"></div>
-            <div class="col-md-offset-2 col-md-8"></div>
-                <div class="col-lg-offset-3 col-lg-6">
-                    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-                    <div class="form-group">
-                        {!! Form::label('status', 'ステータス:') !!}
-                        {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                    </div>
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
     
-                    <div class="form-group">
-                        {!! Form::label('content', 'タスク:') !!}
-                        {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                    </div>
+            <div class="form-group">
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
         
-                    {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
+            {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
         
-                    {!! Form::close() !!}
-                </div>    
+            {!! Form::close() !!}
+        </div>    
     </div>
 
 @endsection
