3b981caa988a8d23b7129310d09fbe0ad83e6e38
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-13 02:56:06 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
